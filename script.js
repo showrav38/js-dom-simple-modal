8eaad8bc53c684388f883ab14ee5e4ca799b9675
showrav38 <showravsaha38@gmail.com>
2021-09-05 00:57:23 +0600

    escape button added

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 'use strict';
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnsCloseModal =  document.querySelector('.close-modal');
+const btnsCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 // console.log(btnsOpenModal);
-const openModal = function(){ 
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-const closeModal = function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-}
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 // select button and open the modal
-for(let i = 0; i<btnsOpenModal.length; i++){
-    btnsOpenModal[i].addEventListener('click',openModal);
+// for(let i = 0; i<btnsOpenModal.length; i++){
+//     btnsOpenModal[i].addEventListener('click',openModal);
+// }
+
+for (const modals of btnsOpenModal) {
+  modals.addEventListener('click', openModal);
 }
 
 // close button of modal
-btnsCloseModal.addEventListener('click', closeModal)
+btnsCloseModal.addEventListener('click', closeModal);
 
 // click anywhere
-overlay.addEventListener('click',closeModal)
+overlay.addEventListener('click', closeModal);
+
+
+document.addEventListener('keydown', function (e) {
+  console.log(e['key']);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+    //   console.log(e);
+  }
+});
