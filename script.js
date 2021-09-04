8791ff3e9fd0b62652d9791dbfc521c9731b34fb
showrav38 <showravsaha38@gmail.com>
2021-09-04 21:40:16 +0600

    modal added

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 'use strict';
-console.log('test');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsCloseModal =  document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+// console.log(btnsOpenModal);
+const openModal = function(){ 
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i = 0; i<btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click',openModal);
+}
+
+
+btnsCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click',closeModal)
